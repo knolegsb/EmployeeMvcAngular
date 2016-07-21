5c7651fc6b32e7f0465caef02331a07a7ab5859f
knolegsb <seon2you@hotmail.com>
2016-07-21 16:57:53 -0700

    first commit and initial model and controller with return json

---
+++ EmployeeMvcAngular/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using EmployeeMvcAngular.Entities;
+using EmployeeMvcAngular.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EmployeeMvcAngular.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAll()
+        {
+            var employeeList = dbContext.Employees.ToList();
+            return Json(employeeList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetEmployeeByNo(string empNo)
+        {
+            int no = Convert.ToInt32(empNo);
+            var employeeList = dbContext.Employees.Find(no);
+            return Json(employeeList, JsonRequestBehavior.AllowGet);
+        }
+
+        public string UpdateEmployee(Employee emp)
+        {
+            if (emp != null)
+            {
+                int no = Convert.ToInt32(emp.ID);
+                var employeeList = dbContext.Employees.Where(e => e.ID == no).FirstOrDefault();
+                employeeList.Name = emp.Name;
+                employeeList.MobileNumber = emp.MobileNumber;
+                employeeList.Email = emp.Email;
+                dbContext.SaveChanges();
+
+                return "Employee Updated";
+            }
+            else
+            {
+                return "Invalid Employee";
+            }
+        }
+
+        public string AddEmployee(Employee emp)
+        {
+            if (emp != null)
+            {
+                dbContext.Employees.Add(emp);
+                dbContext.SaveChanges();
+                return "Employee Updated";
+            }
+            else
+            {
+                return "Invalid Employee";
+            }
+        }
+    }
+}
