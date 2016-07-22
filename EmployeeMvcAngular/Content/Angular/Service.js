17b1f7d2a6bf291aa341a54bb7156701bc91cdc4
knolegsb <seon2you@hotmail.com>
2016-07-22 07:58:33 -0700

    added services

--- EmployeeMvcAngular/Content/Angular/Service.js
+++ EmployeeMvcAngular/Content/Angular/Service.js
@@ -5,7 +5,7 @@
     };
 
     // get Employee By id
-    this.getEmployee = function (employeeID) {
+    this.getEmployee = function(employeeID) {
         var response = $http({
             method: "post",
             url: "Home/GetEmployeeById",
@@ -16,5 +16,37 @@
         return response;
     }
 
+    // Update Employee
+    this.updateEmp = function(employee) {
+        var response = $http({
+            method: "post",
+            url: "Home/UpdateEmployee",
+            data: JSON.stringify(employee),
+            dataType: "json"
+        });
+        return response;
+    }
 
-})
+    // Add Employee
+    this.addEmp = function(employee) {
+        var response = $http({
+            method: "post",
+            url: "Home/AddEmployee",
+            data: JSON.stringify(employee),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    // Delete Employee
+    this.deleteEmp = function(employeeId) {
+        var response = $http({
+            method: "post",
+            url: "Home/DeleteEmployee",
+            params: {
+                employeeId: JSON.stringify(employeeId)
+            }
+        });
+        return response;
+    }
+});
